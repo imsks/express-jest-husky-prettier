b05c8075156a24212868246964fbee50390aeecb
Sachin <sachinkshuklaoo7@gmail.com>
2021-08-10 12:40:39 +0530

    added /docs

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import morgan from 'morgan';
 import BodyParser from 'body-parser';
 import indexRouter from './routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 
@@ -14,5 +16,8 @@ app.use(morgan('combined'));
 
 // Routes
 app.use('/api/v1/', indexRouter);
+
+// For Swagger
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
